6c62eec0c7cb7aac10e9b1ab2e76d895693108a5
Roger Hom <roger.c.hom@gmail.com>
2015-07-22 16:34:40 -0700

    Implement ImagesTableViewController, with images (included in project) scaled properly

---
+++ Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by psytronx on 7/22/15.
+//  Copyright (c) 2015 Logical Dimension. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@end
