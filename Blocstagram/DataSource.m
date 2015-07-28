d3a3e0ccd236d121f3d1614fb6331ab4a3e93aaf
Roger Hom <roger.c.hom@gmail.com>
2015-07-27 20:32:36 -0700

    Make delete user action cause item to be pushed to top of list

--- Blocstagram/DataSource.m
+++ Blocstagram/DataSource.m
@@ -143,6 +143,7 @@
 - (void) deleteMediaItem:(Media *)item {
     NSMutableArray *mutableArrayWithKVO = [self mutableArrayValueForKey:@"mediaItems"];
     [mutableArrayWithKVO removeObject:item];
+    [mutableArrayWithKVO insertObject:item atIndex:0];
 }
 
 @end
