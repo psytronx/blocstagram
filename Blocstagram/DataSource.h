e67a7c4f826a8387ca5b31fe0d6c48e7b8f8370d
Roger Hom <roger.c.hom@gmail.com>
2015-08-20 12:31:36 -0700

    Update the data source to handle like toggle

--- Blocstagram/DataSource.h
+++ Blocstagram/DataSource.h
@@ -29,4 +29,6 @@ typedef void (^NewItemCompletionBlock)(NSError *error);
 - (void) requestOldItemsWithCompletionHandler:(NewItemCompletionBlock)completionHandler;
 - (void) downloadImageForMediaItem:(Media *)mediaItem;
 
+- (void) toggleLikeOnMediaItem:(Media *)mediaItem withCompletionHandler:(void (^)(void))completionHandler;
+
 @end
