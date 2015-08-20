5891a3c394888a54cde71feae428374955f18680
Roger Hom <roger.c.hom@gmail.com>
2015-08-20 11:52:45 -0700

    Update the Media Model to Track Like State

--- Blocstagram/Media.m
+++ Blocstagram/Media.m
@@ -44,7 +44,13 @@
             [commentsArray addObject:comment];
         }
         
+        
+        
         self.comments = commentsArray;
+        
+        // User liked
+        BOOL userHasLiked = [mediaDictionary[@"user_has_liked"] boolValue];
+        self.likeState = userHasLiked ? LikeStateLiked : LikeStateNotLiked;
     }
     
     return self;
@@ -71,6 +77,7 @@
         
         self.caption = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(caption))];
         self.comments = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(comments))];
+        self.likeState = [aDecoder decodeIntegerForKey:NSStringFromSelector(@selector(likeState))];
         
     }
     
@@ -84,6 +91,7 @@
     [aCoder encodeObject:self.image forKey:NSStringFromSelector(@selector(image))];
     [aCoder encodeObject:self.caption forKey:NSStringFromSelector(@selector(caption))];
     [aCoder encodeObject:self.comments forKey:NSStringFromSelector(@selector(comments))];
+    [aCoder encodeInteger:self.likeState forKey:NSStringFromSelector(@selector(likeState))];
 }
 
 @end
